ea94597971dfa83f0c3ee2d62648affdfe1bcf98
Antony <antonygradillas@gmail.com>
2023-08-25 16:16:00 -0700

    Add buttons and code behind for sum totals

--- AvaloniaBillingApp/MainWindow.axaml.cs
+++ AvaloniaBillingApp/MainWindow.axaml.cs
@@ -1,4 +1,7 @@
+using System;
+// using System.Diagnostics;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace AvaloniaBillingApp;
 
@@ -7,5 +10,44 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+    
+    // Summation Button
+    public void ButtonSumCharge(object source, RoutedEventArgs args)
+    {
+        string baseCharge = BaseCharge.Text.Replace("$", "");
+        string ancFees = AncFees.Text.Replace("$", "");
+        string taxes = Taxes.Text.Replace("$", "");
+        string credit = Credit.Text.Replace("$", "");
+        
+        if (Double.TryParse(baseCharge, out var b) &
+            Double.TryParse(ancFees, out var a) &
+            Double.TryParse(taxes, out var t) &
+            Double.TryParse(credit, out var c))
+        {
+            Total.Text = $"${b + a + t - c}";
+        }
+    }
+    
+    // Update paid text box if paid
+    public void ButtonUpdatePaidStatus(object source, RoutedEventArgs args)
+    {
+        try
+        {
+            if (PaidDate.SelectedDate.HasValue &
+                (PaidDate.SelectedDate.GetValueOrDefault() - BilledDate.SelectedDate.GetValueOrDefault()).TotalDays >= 0)
+            {
+                PaidBool.Text = "TRUE";
+            }
+        }
+        catch (Exception err)
+        {
+            Console.WriteLine(err);
+        }
+        finally
+        {
+            Console.WriteLine($"Entered Date: {PaidDate.SelectedDate.GetValueOrDefault()}");
+            Console.WriteLine($"Entered Total: {Total.Text}");
+        }
     }
 }
